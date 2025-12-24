f643d0877fb800731227e683210187431fe6c092
Gs_Sidhu <102853351+guru-desi@users.noreply.github.com>
2025-12-24 16:19:52 +0530

    Create Assign_2__Task_2.py

---
+++ Assign_2__Task_2.py
@@ -0,0 +1,14 @@
+# Assign 2 Task 1 
+# Sum of intergers from 1 to 50 using loop
+
+# define variabels 
+sum = 0
+
+# logic Implimentation 
+
+for i in range(1,51):
+    sum = sum + i
+    
+# print On Display 
+    
+print("The sum of Numbers from 1 to 50 is : ",sum)    
