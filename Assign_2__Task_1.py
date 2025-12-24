4330105e3019bb93d28659f46c627e6ea7e81849
Gs_Sidhu <102853351+guru-desi@users.noreply.github.com>
2025-12-24 16:19:15 +0530

    Create Assign_2__Task_1.py

---
+++ Assign_2__Task_1.py
@@ -0,0 +1,16 @@
+# Assign 2 Task 1 
+# Check If a Number is Even Or Old  
+
+# Define variable to store user input
+
+Number = int(input("Please Enter a Number : "))
+
+# Logic Implimentation Using if Else to Find Out Number is Even or Old 
+
+if(Number % 2 == 0):
+    print(f"{Number} is an Even Number")
+else:
+    print(f"{Number} is an Old Number")
+
+
+ 
